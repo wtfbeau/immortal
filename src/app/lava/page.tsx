8adc46ed1d560c8a0d05b43ed1d7186c8a3f8dcb
Giorgi Kyn <giorgikyn@Giorgis-MacBook-Pro.local>
2025-04-25 10:36:09 +0400

    copychange2

--- src/app/lava/page.tsx
+++ src/app/lava/page.tsx
@@ -1,5 +1,4 @@
 import BeforeAfterSection from '../lavaSections/BeforeAfterSection';
-import FaqSection from '../lavaSections/FaqSection';
 import FinalCTASection from '../lavaSections/FinalCtaSection';
 import Footer from '../lavaSections/Footer1';
 import Header from '../lavaSections/Header';
@@ -46,7 +45,6 @@ export default function Home() {
 
       {/* Final CTA Section */}
       <FinalCTASection id="final-cta" />
-      <FaqSection />
 
       {/* Footer */}
       <Footer />
